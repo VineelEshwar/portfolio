2a774a388b0c0e8287df1c04248483502c7198ee
B M Vineel Eshwar <vineelesh123@gmail.com>
2023-11-26 18:01:38 +0530

    portfolio end

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function toogleMenu() {
-const menu= document.querySelector(".menu-links");
-const icon= document.querySelector(".hamburger-icon");
-menu.classList.toggle("open");
-icon.classList.toggle("open");
-} 
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
